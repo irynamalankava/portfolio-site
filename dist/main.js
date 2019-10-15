f193e7efdf5dab6a447984c44d4398e8ce95bc11
ira <ira@ira>
2019-10-14 21:41:37 -0500

    add header name toggle on scroll functionality

--- dist/main.js
+++ dist/main.js
@@ -1,45 +1,48 @@
 //window.onload()
-//Menu button toogle 
+
 
 const menuBtn  = document.querySelector('.menu-btn');
-
 const menuNav  = document.querySelector('.menu');
 const navItems  = document.querySelectorAll('.nav-item');
+const sections = document.querySelectorAll('section');
 
 
-//Making header fixed after scrolling
-function changeLinkState() {
+//adding active class to nav items after scroll
+function changeNavItemState() {
   let index = sections.length;
+  const offset = 50;
 
-  while(--index && window.scrollY + 100 < sections[index].offsetTop) {
+  while(--index && window.scrollY + offset < sections[index].offsetTop) {
     
   }
   navItems.forEach((item) => item.classList.remove('active'));
-  navItems[index].classList.add('active');
-  
-  
+  navItems[index].classList.add('active');  
 }
+
+//Making header fixed after scrolling
 function fixedHeader(){
   const nav = document.querySelector('header');
   if(this.scrollY <= 15) nav.className = ''; 
   else nav.className = 'scroll';
 };
 
-//changeLinkState();
-/*window.onscroll= function(){
-  changeLinkState();
-  fixedHeader();
-}*/
-window.addEventListener('scroll', changeLinkState);
-
-
-window.addEventListener('scroll', fixedHeader);
-
 //Making Name moving on the nav bar after scrolling
-/*window.onscroll = () => {
-    const name = document.querySelector('.moving-name');
-    if(this.scrollY >= 15) {name.classList.add('scroll1');
-  }};*/
+function changeNameVisibility(){
+  const name = document.getElementById('header-name');
+  this.scrollY >= 15 ?
+    name.style.visibility = "visible":
+    name.style.visibility = "hidden"  
+};
+
+window.onscroll= function(){
+  changeNavItemState();
+  fixedHeader();
+  changeNameVisibility();
+}
+//window.addEventListener('scroll', changeLinkState);
+//window.addEventListener('scroll', fixedHeader);
+
+
 
 
 
@@ -50,6 +53,7 @@ let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
+//Menu button toogle 
 function toggleMenu() {
     if(!showMenu){
         menuBtn.classList.add('close');
@@ -60,7 +64,7 @@ function toggleMenu() {
         showMenu = true;
     }
     else{
-        menuBtn.classList.toggle('ccc1') .remove('close');
+        menuBtn.classList.remove('close');
         menuNav.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
         
@@ -69,18 +73,6 @@ function toggleMenu() {
     }
 
 }
-//adding active class to nav items onscroll
-//const navItems = document.querySelectorAll('.nav-item');
-const sections = document.querySelectorAll('section');
 
 
 
-//changing LinkedIn icon class in media query max-width: 576px
-
-/*window.onresize = () =>{
-    const linkedInIcon = document.querySelector("#linkedIn1");
-    if (window.width()<=576){
-        linkedInIcon.classList.remove('fa-3x');
-        linkedInIcon.classList.add('fa-2x');
-    }
-}*/
